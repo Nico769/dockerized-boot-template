8de080c5d6206cf70e35dd6f1814b2aa0cbcff36
Nico769 <ncl.lnd769@gmail.com>
2022-04-30 12:50:53 +0200

    add initializr template
    - Spring Boot starters: web, data, validation, devtools, actuator
    - Database drivers: H2, PostgreSQL
    - Google's JIB based on Amazon Corretto SDK 17-alpine

---
+++ src/main/java/com/nico/dockerizedboottemplate/DockerizedBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.nico.dockerizedboottemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizedBootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizedBootTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nico/dockerizedboottemplate/DockerizedBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nico.dockerizedboottemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizedBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
